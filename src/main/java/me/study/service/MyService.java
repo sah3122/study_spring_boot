9921eacf3a90879bc256a9a7e869f406e33daa22
dongchul <sah3122@naver.com>
2019-09-18 21:54:43 +0900

    ADD Component & Functional Bean Inject

---
+++ src/main/java/me/study/service/MyService.java
@@ -0,0 +1,4 @@
+package me.study.service;
+
+public class MyService {
+}
--- src/main/java/me/study/springbootautowired/SpringbootautowiredApplication.java
+++ src/main/java/me/study/springbootautowired/SpringbootautowiredApplication.java
@@ -1,13 +1,27 @@
 package me.study.springbootautowired;
 
+import me.study.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootApplication
 public class SpringbootautowiredApplication {
 
+    @Autowired
+    MyService myService;
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootautowiredApplication.class, args);
+        //SpringApplication.run(SpringbootautowiredApplication.class, args);
+        var app = new SpringApplication(SpringbootautowiredApplication.class);
+        app.addInitializers((ApplicationContextInitializer< GenericApplicationContext>) ctx -> {
+            ctx.registerBean(MyService.class);
+            ctx.registerBean(ApplicationRunner.class, () -> args1 -> System.out.println("Functional Bean Definition"));
+        });
+        app. run(args);
     }
 
 }
