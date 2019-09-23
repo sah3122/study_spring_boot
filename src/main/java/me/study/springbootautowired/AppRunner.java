149408f4e8c13d4dc555ae142ec68901cf9cd889
dongchul <sah3122@naver.com>
2019-09-23 22:30:13 +0900

    ADD ResourceLoader get class

--- src/main/java/me/study/springbootautowired/AppRunner.java
+++ src/main/java/me/study/springbootautowired/AppRunner.java
@@ -3,6 +3,7 @@ package me.study.springbootautowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -17,11 +18,14 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println(resourceLoader.getClass());
+
         Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.getClass());
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(Files.readString(Path.of(resource.getURI())));
