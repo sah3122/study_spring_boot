e4437939420c316f43481a4020117c601efcfe24
dongchul <sah3122@naver.com>
2019-09-23 13:02:51 +0900

    ADD ResourceLoader

--- src/main/java/me/study/springbootautowired/AppRunner.java
+++ src/main/java/me/study/springbootautowired/AppRunner.java
@@ -3,24 +3,27 @@ package me.study.springbootautowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.context.MessageSource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
-import java.util.Locale;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 /**
- * Created by dongchul on 2019-09-20.
+ * Created by dongchul on 2019-09-23.
  */
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher eventPublisher; // 이벤트를 넘겨주기 위한 객체
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        eventPublisher.publishEvent(new MyEvent(this, 100));
-
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        System.out.println(Files.readString(Path.of(resource.getURI())));
     }
 }
